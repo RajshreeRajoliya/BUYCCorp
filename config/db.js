c3460c7524be24c30edf78ad351b286d78973385
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-07-07 22:44:59 +0530

    UpdateConnectionString

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 async function Connection(username, password) {
-  const URL = `mongodb+srv://${username}:${password}@buycar.dd1hxbx.mongodb.net/?retryWrites=true&w=majority`;
+  const URL = `mongodb://${username}:${password}@ac-jdcsfa4-shard-00-00.dd1hxbx.mongodb.net:27017,ac-jdcsfa4-shard-00-01.dd1hxbx.mongodb.net:27017,ac-jdcsfa4-shard-00-02.dd1hxbx.mongodb.net:27017/?ssl=true&replicaSet=atlas-dsc0iw-shard-0&authSource=admin&retryWrites=true&w=majority`;
   try {
     await mongoose.connect(URL, { useNewUrlParser: true,
       useUnifiedTopology: true, });
